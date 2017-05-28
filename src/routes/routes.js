67d347c0a91bc0bb9374f95492353c16e557aadb
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-05-28 09:32:22 -0400

    Create form template and route to add new poll

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,14 @@ module.exports = function(app, env) {
 		res.sendFile(path.join(__dirname + '/../../public/index.html'));
 	});
 
+	app.get('/createPoll', function(req, res) {
+		res.sendFile(path.join(__dirname + '/../../public/addPoll.html'));
+	});
+
+	app.post('/createPoll', function(req, res) {
+		console.log("createPoll post...");
+	});
+
 	app.get('*', function(req, res) {
 		res.redirect('/');
 	});
