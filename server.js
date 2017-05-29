296645a54e6eff4f4e5a2b227cdb4c7396505112
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-05-29 18:42:43 -0400

    Add mongodb connection to project

--- server.js
+++ server.js
@@ -3,10 +3,27 @@ var routes = require('./src/routes/routes.js');
 
 var app = express();
 
-routes(app, process.env);
+var MongoClient = require('mongodb').MongoClient;
+var dbURL = process.env.MONGOLAB_URL;
 
-app.set('port', (process.env.PORT || 8080));
+MongoClient.connect((dbURL || 'mongodb://localhost:27017/votingdb'), function(err, db) {
+	if(!err) {
+		console.log("Connected to database.");
+	}
+	else if(err) {
+		console.log(err);
+	}
 
-app.listen(process.env.PORT || 8080, function() {
-	console.log("Server Listening on Port 8080");
+	app.use(function(req, res, next) {
+		req.db = db;
+		next();
+	});
+
+	routes(app, process.env);
+
+	app.set('port', (process.env.PORT || 8080));
+
+	app.listen(process.env.PORT || 8080, function() {
+		console.log("Server Listening on Port 8080");
+	});
 });
